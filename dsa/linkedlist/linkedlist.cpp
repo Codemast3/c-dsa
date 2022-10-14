833359e4ae8943b76cf78a0db1274be3f467c3d0
Gaurav <gauravbhagat326@gmail.com>
2022-10-14 15:17:47 +0530

    recusion and link list

---
+++ dsa/linkedlist/linkedlist.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+} ;
+int main (){
+    struct node* head = NULL;
+    struct node* second = NULL;
+    struct node* third = NULL;
+    head = new node;
+    second = new node;
+    third = new node;
+    
+
+    
+        /* data */
+    
+    
+
+    
+        /* data */
+    
+    
+}
